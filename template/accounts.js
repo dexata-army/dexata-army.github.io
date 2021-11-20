490da3b33fe197af652b67e6c76726c772d79cca
n-ham <nicholas.charles.ham@gmail.com>
2021-11-21 10:00:16 +1100

    added accounts, multiple watchlists, multiple wallets, lots of networks for wallets, basic ignorelist functionality for wallets implemented

---
+++ template/accounts.js
@@ -0,0 +1,91 @@
+<script>
+	function logout()
+	{
+		localStorage.removeItem('no_dexata_tabs');
+		localStorage.removeItem('last_load_time');
+		localStorage.removeItem('username');
+		sessionStorage.removeItem('username');
+		localStorage.removeItem('password');
+		sessionStorage.removeItem('password');
+		location.reload();
+	}
+
+	function check_for_logout(username)
+	{
+		sleep(2000).then(() => { 
+			if(!localStorage.getItem('username') || 
+				localStorage.getItem('username') != username)
+				location.reload();
+			else
+				check_for_logout(username);
+		});
+	}
+
+	var session_timeout_sec = localStorage.getItem(user + '-session_timeout')*60;
+
+	if(session_timeout_sec != undefined && session_timeout_sec > 0)
+	{
+		if(sessionStorage.getItem('username') || localStorage.getItem('username'))
+		{
+			var curr_time = Math.round((new Date()).getTime() / 1000);
+
+			//checks last page load time
+			if(localStorage.getItem("last_load_time"))
+			{
+				if(curr_time - localStorage.getItem("last_load_time") > session_timeout_sec)
+					logout();
+				else
+					localStorage.setItem("last_load_time", curr_time);
+			}
+			else
+				logout();
+		}
+	}
+
+	if(sessionStorage.getItem("username") && !localStorage.getItem("username"))
+	{
+		localStorage.setItem("no_dexata_tabs", 0);
+		localStorage.setItem("username", sessionStorage.getItem("username"));
+		localStorage.setItem("password", sessionStorage.getItem("password"));
+	}
+	else if(!sessionStorage.getItem("username") && localStorage.getItem("username"))
+	{
+		sessionStorage.setItem("username", localStorage.getItem("username"));
+		sessionStorage.setItem("password", localStorage.getItem("password"));
+	}
+
+	var load_host = window.location.hostname;
+	if(localStorage.getItem("username"))
+	{
+		//updates number of dexata tabs open
+		if(localStorage.getItem('no_dexata_tabs') != undefined &&
+			localStorage.getItem('no_dexata_tabs') >= 0)
+			localStorage.setItem('no_dexata_tabs', parseFloat(localStorage.getItem('no_dexata_tabs'))+1);
+		else
+			localStorage.setItem('no_dexata_tabs', 1);
+
+		window.addEventListener('beforeunload', function (e) {
+			if(localStorage.getItem('no_dexata_tabs'))
+			{
+			    localStorage.setItem('no_dexata_tabs', parseFloat(localStorage.getItem('no_dexata_tabs'))-1);
+
+			    if(localStorage.getItem('no_dexata_tabs') == 0)
+			    {
+			    	localStorage.removeItem('no_dexata_tabs');
+			    	localStorage.removeItem('username');
+			    	localStorage.removeItem('password');
+			    }
+			}
+			else
+			{
+				sessionStorage.removeItem('username');
+				sessionStorage.removeItem('password');
+			}
+		});
+
+		//reloads page if user has logged out
+		$(document).ready(function(){
+			check_for_logout(localStorage.getItem('username'));
+		});
+	}
+</script>
---
+++ template/toggle_login.js
@@ -0,0 +1,13 @@
+<script>
+	//changes login to logout on page if logged in
+	if(localStorage.getItem("username"))
+	{
+		document.getElementById('login_menu_item').innerHTML = "<a onclick='logout()'>logout</a>";
+		document.getElementById('login_footer_item').innerHTML = "<a onclick='logout()' title='logout'><i class='fas fa-sign-out-alt fa-2x'></i></a>";
+	}
+	else
+	{
+		document.getElementById('login_menu_item').innerHTML = "<a href='@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'>login</a>"
+		document.getElementById('login_footer_item').innerHTML = "<a href='@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'><i class='fas fa-sign-in-alt fa-2x'></i></a>"
+	}
+</script>
