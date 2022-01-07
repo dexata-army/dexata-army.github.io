aed0aeffd46c76298c930f341edf847a32381370
n-ham <nicholas.charles.ham@gmail.com>
2022-01-07 16:33:49 +1100

    changed website design

--- template/toggle_login.js
+++ template/toggle_login.js
@@ -1,16 +1,16 @@
 <script>
-	//changes login to logout on page if logged in
+	//sets up login/logout menu link
 	if(localStorage.getItem("username"))
 	{
-		//document.getElementById('login_menu_item').innerHTML = "<a onclick='logout()'>logout</a>";
-		//document.getElementById('login_footer_item').innerHTML = "<a onclick='logout()' title='logout'><i class='fas fa-sign-out-alt fa-2x'></i></a>";
-		document.getElementById('login_menu_item').innerHTML = "<a href='@pathto(logout)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'>logout</a>"
-		document.getElementById('login_footer_item').innerHTML = "<a href='@pathto(logout)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'><i class='fas fa-sign-out-alt fa-2x'></i></a>"
+		document.getElementById('login_menu_item').innerHTML = "<a href='@pathto(logout)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "' class='menu-link'><span class='menu-icon'><i class='fas fa-sign-out-alt fa-1x'></i></span><span class='menu-text'>logout</span></a>";
+		document.getElementById('login_header_item').innerHTML = "logout <i class='fas fa-sign-out-alt fa-1x ms-auto text-theme fs-16px my-n1'></i>";
+		document.getElementById('login_header_item').href = "@pathto(logout)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
 	}
 	else
 	{
-		document.getElementById('login_menu_item').innerHTML = "<a href='@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'>login</a>"
-		document.getElementById('login_footer_item').innerHTML = "<a href='@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "'><i class='fas fa-sign-in-alt fa-2x'></i></a>"
+		document.getElementById('login_menu_item').innerHTML = "<a href='@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1) + "' class='menu-link'><span class='menu-icon'><i class='fas fa-sign-in-alt fa-1x'></i></span><span class='menu-text'>login</span></a>";
+		document.getElementById('login_header_item').innerHTML = "login <i class='fas fa-sign-in-alt fa-1x ms-auto text-theme fs-16px my-n1'></i>";
+		document.getElementById('login_header_item').href = "@pathto(login)?return_to=" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
 	}
 
 	//prevent autocomplete
